ac03da6b55f3811cb57e1181ba526e8b6c43869c
chinnajee <chinnajee.raja@gmail.com>
2022-07-21 21:24:23 +0530

    updated class name as hello1

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
-class Hello{  
+class Hello1{  
     public static void main(String args[]){  
-     System.out.println("Hello from Chinna");  
+     System.out.println("Hello from Exclr");  
     }  
 } 
